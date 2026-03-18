364b2f4479df5972cc0d54626932f3579b0da5e3
TeresaWanjiku <ndirangutess@gmail.com>
2026-03-18 16:28:26 +0300

    test: add TC02 schema validation on initialize transaction

--- src/test/java/TransactionTest.java
+++ src/test/java/TransactionTest.java
@@ -18,4 +18,21 @@ public class TransactionTest extends BaseTest {
                 .body("status", equalTo(true))
                 .body("data.reference", notNullValue());
     }
+
+    @Test
+    public void TC02_shouldReturnValidSchemaOnInitialize() {
+        given()
+                .header("Authorization", "Bearer " + SECRET_KEY)
+                .contentType(ContentType.JSON)
+                .body("{\"email\": \"test@example.com\", \"amount\": \"10000\"}")
+                .when()
+                .post("/transaction/initialize")
+                .then()
+                .statusCode(200)
+                .body("status", equalTo(true))
+                .body("message", equalTo("Authorization URL created"))
+                .body("data.authorization_url", notNullValue())
+                .body("data.access_code", notNullValue())
+                .body("data.reference", notNullValue());
+    }
 }
